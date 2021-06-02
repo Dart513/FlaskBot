fb33531714d9b8b0c5b578be0180a4dc20b72d6a
Glazed_Darnut <dartonaili@gmail.com>
2021-06-02 19:10:52 -0400

    Improved verification mechanism

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fdm = new (require('./flatDataManager'));
 
-const readline = require('readline');
 
+const verify = new (require('./verify'));
+
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -14,7 +16,11 @@ const rl = readline.createInterface({
 });
 
 
-//Log everything
+//Checks if a string matches an image
+const isImage = new RegExp("(png|jpg|jpeg)(?!\.)", "gmi");
+
+
+//Startup everything when the bot-server connection has been made
 client.once('ready', () => {
     console.log("Ready!");
     client.user.setActivity(`over tronlings`, { type: 'WATCHING' })
@@ -33,7 +39,7 @@ client.once('ready', () => {
 });
 
 
-
+//When the bot is invited into a new guild, we make sure we set up everything properly. 
 client.on('guildCreate', (guild) => {
     //Set up the guild in storage
     fdm.addGuild(guild.id, guild.name);
@@ -41,46 +47,80 @@ client.on('guildCreate', (guild) => {
 
 
 //TODO: Process commands to slash commands when everything is done.
-
 client.on('message', processMsg);
 
-//Code for main substr replacement operation
+
+/**
+ * Deal with messages when they come in
+ * @param {Message} message 
+ */
 async function processMsg(message) {
     let content = message.cleanContent;
     //if (message.channel.name == 'general') console.log("Message");
     if (!message.author.bot) {
 
-        let replacements = fdm.getReplacementsForGuild(message.guild.id);
+        //DMs
+        if (message.channel instanceof Discord.DMChannel) {
+            if (message.attachments.size > 0) {
 
-        
-        //Make sure to only match fragments first
-        let priorityReplacements = replacements.filter(element => !element.options.replacePartial);
-        
-        let success = (await Promise.all(priorityReplacements.map(async (element) => {
-            if (content.search(element.regex) > -1) {
-                message.channel.send(element.result);
-                return true;
+                //Iterate over each attachment
+                message.attachments.each(async attachment => {
+                    //Checks if the attachment is an image
+                    console.log(content);
+                    if (isImage.test(attachment.name) && content.length > 0) {
+
+                        //Tell the user that we're verifying.
+                        message.channel.send("Verifying ... Please note this may take a while.");
+
+                        let verified = await verify.verify(attachment.url, content);
+
+
+                        //Return a verified/non-verified value.
+                        message.channel.send(verified ? "You are verified!" : "That was not valid verification.");
+
+                    } else {
+                        console.log(JSON.stringify(attachment));
+                        message.channel.send("To verify, send a full screenshot of your quest admissions screen, along with your full name. If you already have, please try again with a high-res image.");
+                    }
+                });
+            } else {
+                message.channel.send("To verify, send a full screenshot of your quest admissions screen, along with your full name. If you already have, please try again with a high-res image.");
             }
-            else return false;
-        }))).filter(element => element === true).length > 0;
-
-
-        //Now check for any replacement matches which match fragments
-        if (!success) {
-            priorityReplacements = replacements.filter(element => element.options.replacePartial); //Now check for fragment replacement matches
-            
-            content = ` ${content} `;
-            oldContent = content;
-
-            await Promise.all(priorityReplacements.map(async (element) => {
-
-                if (content.search(element.regex) > -1) {
-                    content = content.replace(element.regex, element.result);
-                }
-            }));
-
-            if (content !== oldContent) message.channel.send(content);
         }
+        //Guild messages
+        else {
+            let replacements = fdm.getReplacementsForGuild(message.guild.id);
+        
+            //Make sure to only match fragments first
+            let priorityReplacements = replacements.filter(element => !element.options.replacePartial);
+            
+            let success = (await Promise.all(priorityReplacements.map(async (element) => {
+                if (content.search(element.regex) > -1) {
+                    message.channel.send(element.result);
+                    return true;
+                }
+                else return false;
+            }))).filter(element => element === true).length > 0;
+    
+    
+            //Now check for any replacement matches which match fragments
+            if (!success) {
+                priorityReplacements = replacements.filter(element => element.options.replacePartial); //Now check for fragment replacement matches
+                
+                content = ` ${content} `;
+                oldContent = content;
+    
+                await Promise.all(priorityReplacements.map(async (element) => {
+    
+                    if (content.search(element.regex) > -1) {
+                        content = content.replace(element.regex, element.result);
+                    }
+                }));
+    
+                if (content !== oldContent) message.channel.send(content);
+            }
+        }
+
     }
 }
 
@@ -102,6 +142,7 @@ process.on("SIGINT", () => {
     console.log("Shutting down");
     fdm.close();
     rl.close();
+    verify.close();
 
     client.destroy();
 });
--- verify.js
+++ verify.js
@@ -1,6 +1,7 @@
 /**
  * A singleton class to export. Verifies a user from a screenshot of their quest info. Does not require Tesseract to be re-initialized each time it is run.
  */
+const path = require('path');
 
 class Verify {
 
@@ -16,55 +17,120 @@ class Verify {
 
     //Initialize the only Tesseract.
     async init() {
-      const createWorker = require('tesseract.js').createWorker;
+      this.fs = require('fs');    
+      //Make sure the training data was not corrupted
+      this.fs.copyFileSync('./engBackup.traineddata', './eng.traineddata');
 
-      const worker = createWorker({
-        logger: m => console.log(m)
-      });
+      const Tesseract = require('tesseract.js');
+
+      const { createScheduler } = Tesseract;
+      const { createWorker } = Tesseract;
+
+      const scheduler = createScheduler();
+
+      this.https = require('https')                                            
+      this.Stream = require('stream').Transform                                 
       
-      await worker.load();
-      await worker.loadLanguage('eng');
-      await worker.initialize('eng');
-      const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-      console.log(text);
-      await worker.terminate();
+      this.sharp = require('sharp');
+
+
+
+
+      //We're not expecting very many requests coming in per second, we're only going to have 2 workers.
+      let workers = [createWorker({
+        //logger: m => console.log(m)
+      }),
+      createWorker({
+        //logger: m => console.log(m)
+      })];
+
+      // Init the workers and add them to the scheduler.
+      await Promise.all(workers.map(async worker => {
+        await worker.load();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+
+        scheduler.addWorker(worker)
+      }));
       
-        
+      this.scheduler = scheduler;
+      this.workers = workers;
     }
 
-    async recognize() {
+    /**
+     * Runs OCR on the image
+     * @param {String} img url to image 
+     * @returns the text from the image
+     */
+    async recognize(img) {
+
+      //grab the image
+
+      let id = Math.random();
+      let tempFile = path.join(__dirname, '/data/images/', `${id}.png`);
+      await this.sharp(await this._fetchImage(img))
+        .sharpen(5, 1.5, 2)
+        .resize(2000)
+        .toFile(tempFile);
+      
       await this.isInitialized;
-      const { data: { text } } = await this.worker.recognize('https://ptb.discord.com/channels/826117425387929661/835893922085339166/849709092582195201');
-      console.log(text);
+      const { data: { text } } = await this.scheduler.addJob('recognize', tempFile);
+      console.log("Text:", text);
+
+      this.fs.unlink(tempFile, () => {});
+      
+      return text;
     }
 
+
+    /**
+     * Fetches an image from a url.
+     * @param {String} url 
+     * @returns Buffer
+     */
+    async _fetchImage(url) {
+      let self = this;
+      return new Promise( (resolve, reject) => {
+        self.https.request(url, function(response) {                                        
+          var data = new self.Stream();                                                    
+        
+          response.on('data', function(chunk) {                                       
+            data.push(chunk);                                                         
+          });                                                                         
+        
+          response.on('end', function() {                                             
+            resolve(data.read());                              
+          });                                                                         
+        }).end();
+      });
+    }
+
+    
+    /**
+     * Checks if the image matches an intention to matriculate
+     * @param {String} img url to image
+     * @param {String} name name of person
+     * @returns true/false
+     */
+    async verify(img, name) {
+      const text = await this.recognize(img);
+      const isVerification = new RegExp(`((?=.*?matriculate.*?)(?=.*?intention.*?)(?=.*?mechatronics.*?)(?=.*?honours.*?)(?=.*?co-op.*?)(?=.*?${name}.*?))`, 'gmsi');
+      return (isVerification.test(text));
+    }
+
+    /**
+     * Shuts everything down.
+     */
     async close() {
         await this.isInitialized;
-        await this.worker.terminate();
+        await Promise.all[this.workers.map(async worker => await worker.terminate())];
+
     }
 
 }
 
-let temp = new Verify();
-//temp.recognize();
-temp.close();
-
 module.exports = Verify; 
 
+//let temp = new Verify();
 
-/*
-const createWorker = require('tesseract.js').createWorker;
-
-const worker = createWorker({
-  logger: m => console.log(m)
-});
-
-(async () => {
-  await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-  const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-  console.log(text);
-  await worker.terminate();
-})();
-*/
+//temp.recognize('https://cdn.discordapp.com/attachments/835893922085339166/849748985816285194/unknown.png').then(res => console.log(res)).then(res => temp.close());
